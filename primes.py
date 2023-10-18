4120e69da34f87b4cf9b8b5eac27050065e5f1b1
Malik Khan <malik.khan@kcl.ac.uk>
2023-10-18 19:41:47 +0100

    Finished

--- primes.py
+++ primes.py
@@ -1,6 +1,33 @@
-"""List of prime numbers generator."""
+"""List of prime numbers' generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    i = 2
+    while len(list) < number_of_primes:
+        if is_prime(i):
+            list.append(i)
+        i += 1
+
+
+
     return list
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
